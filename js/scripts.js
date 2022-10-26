f744b54291c8e96154deb39c7e5d6c13ec400306
Lucas Soares <108234422+LucasSoares2810@users.noreply.github.com>
2022-10-26 15:02:01 -0300

    Gerando as oitavas

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,35 @@ class CopaDoMundo {
         });
 
         $('#openModal').on('click', function(e){
-            copaDoMundo.mostrarModal();
+            copaDoMundo.mostrarModal(1);
+        })
+
+        $('#openModal2').on('click', function(e){
+            copaDoMundo.mostrarModal(2);
+        })
+
+        $('#openModal3').on('click', function(e){
+            copaDoMundo.mostrarModal(3);
+        })
+
+        $('#openModal4').on('click', function(e){
+            copaDoMundo.mostrarModal(4);
+        })
+
+        $('#openModal5').on('click', function(e){
+            copaDoMundo.mostrarModal(5);
+        })
+
+        $('#openModal6').on('click', function(e){
+            copaDoMundo.mostrarModal(6);
+        })
+
+        $('#openModal7').on('click', function(e){
+            copaDoMundo.mostrarModal(7);
+        })
+
+        $('#openModal8').on('click', function(e){
+            copaDoMundo.mostrarModal(8);
         })
 
         $('#closeModal').on('click', function(e){
@@ -40,12 +68,15 @@ class CopaDoMundo {
                 'git-user': 'LucasSoares2810'
             },
         }).done(function(response){
-            copaDoMundo.timesObj = response.Result;      
+            copaDoMundo.timesObj = response.Result;   
+            
         });
         
         this.timesObj.forEach(paisObj => {
             this.timesInicial.push(paisObj.Name);
         });  
+
+        
     }
   
     // Metodo para pegar os países aleatóriamente
@@ -59,6 +90,8 @@ class CopaDoMundo {
             this.nomes.push(times[numero]);
             times = times.filter(time => time != times[numero]);
         }
+
+    
     }
 
     // Método para preencher o HTML
@@ -69,6 +102,8 @@ class CopaDoMundo {
             let idTime = 'time' + (index+1);
             document.getElementById(idTime).innerHTML = this.nomes[index];
         }
+
+
     }
 
     // Método para tirar os grupos da parte de sorteio para o chaveamento
@@ -83,14 +118,79 @@ class CopaDoMundo {
         
     }
 
-    mostrarModal(){
-        Modal.classList.toggle("hide");
+    mostrarModal(botao){
+        if(botao == 1){
+            this.gerarRodada(0, 3);
+        }
+
+        if(botao == 2){
+            this.gerarRodada(4, 7);
+        }
+
+        if(botao == 3){
+            this.gerarRodada(8, 11);
+        }
+
+        if(botao == 4){
+            this.gerarRodada(12, 15)
+        }
+
+        if(botao == 5){
+            this.gerarRodada(16, 19)
+        }
+
+        if(botao == 6){
+            this.gerarRodada(20, 23)
+        }
+
+        if(botao == 7){
+            this.gerarRodada(24, 27)
+        }
+
+        if(botao == 8){
+            this.gerarRodada(28, 31)
+        }
+        
         fade.classList.toggle("hide");
+        Modal.classList.toggle("hide");
+        
     }
 
+    fecharModal(){
+        Modal.classList.add("hide")
+        fade.classList.add("hide");
+        
+    }
+
+   gerarRodada(inicio, fim){
+
+        let timeCopa = [];
+
+        for(let x=inicio; x<=fim; x++){
+
+            timeCopa.push(this.nomes[x]);
+        }
+
+        for(let y=0; y<=3; y++){
+            
+            for(let i=0; i<=3; i++){
+                let time = '#time' + (i+1) + 'Rodada' + (y+1);
+                $(time).html(timeCopa[i]);
+            }
+        }
+    
+        console.log(this.nomes);
+
+    } 
+
 }
+
 
 // Quando terminar de carregar a pagina, inicia a classe
 window.addEventListener('load', (event) => {
     let copaDoMundo = new CopaDoMundo();
+    
 });
+
+
+
